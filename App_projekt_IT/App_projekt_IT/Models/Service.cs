549ae412b39d600fa13ce44d929ce93337cf139f
adaluk <adlukasik@student.agh.edu.pl>
2026-06-07 21:44:52 +0200

    Dodanie zdjec i opisów do usług

--- App_projekt_IT/App_projekt_IT/Models/Service.cs
+++ App_projekt_IT/App_projekt_IT/Models/Service.cs
@@ -11,6 +11,14 @@ namespace App_projekt_IT.Models
 
         public bool IsNFZ { get; set; }
 
+       
+        [MaxLength(2000, ErrorMessage = "Opis nie może przekraczać 2000 znaków.")]
+        public string? Description { get; set; } 
+        
+        public string? ImagePath { get; set; } 
+
+        public bool IsHighlighted { get; set; } = false; 
+
         public ICollection<Doctor> Doctors { get; set; } = new List<Doctor>();
     }
 }
